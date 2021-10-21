1a6fb73b847b972fbf2af1907fe0c3b28729da5d
Cameron <camerondonner63@gmail.com>
2021-10-21 14:36:51 -0500

    Added five round match play, win/lose screen, function descriptions

--- rps.js
+++ rps.js
@@ -1,66 +1,129 @@
-// You want to play a game of rock paper scissors
-// You find a friend (computer) to play with
+/*	
+Function: playRound
+Goals: 		Compare the two choices that the player and computer picked for their weapon.
+					Determine if the player wins, loses, or draws. Return that result as output.
+Inputs: 	String containing the player's choice of rock, paper, scissors.
+					String containing the computer's choice of rock, paper, scissors.
+Outputs: 	Updated win totals for player and computer.
+Steps:		Function receives player and computer weapon choices as input.
+					Using the player's choice as the basis of comparison, begin comparing inputs.
+					After determining the result of a round, alert the user of that result and
+					the current total wins for player and computer.
+					Return updated win totals as output.
+*/
 function playRound (playerWeaponChoice, computerWeaponChoice) {
   if (playerWeaponChoice === "rock") {
     if (computerWeaponChoice === "rock") {
-      return "That was a tie! You both picked rock. Find a nice canyon to spend some time together in.";
+      alert (`That was a tie! You both picked rock. Find a nice canyon to spend some time together in.
+Results as of round ${ round }. Current score: ${ playerWinTotal } to ${ computerWinTotal }`);
+			return;
     }
     else if (computerWeaponChoice === "paper") {
-      return "You lose! Rocks are actually pretty claustrophobic...";
+			computerWinTotal++;
+      alert (`You lose! Rocks are actually pretty claustrophobic...
+Results as of round ${ round }. Current score: ${ playerWinTotal } to ${ computerWinTotal }`);
+			return computerWinTotal;
     }
     else {
-      return "You win! Smash those scissors to bits!";
+			playerWinTotal++;
+      alert (`You win! Smash those scissors to bits!
+Results as of round ${ round }. Current score: ${ playerWinTotal } to ${ computerWinTotal }`);
+			return playerWinTotal;
     }
   }
   else if (playerWeaponChoice === "paper") {
     if (computerWeaponChoice === "rock") {
-      return "You win! Smother the life out of that pebble!";
+			playerWinTotal++;
+      alert (`You win! Smother the life out of that pebble!
+Results as of round ${ round }. Current score: ${ playerWinTotal } to ${ computerWinTotal }`);
+			return playerWinTotal;
     }
     else if (computerWeaponChoice === "paper") {
-      return "That was a tie! You both picked paper. Find a nice manila folder to spend some time with your new friend in.";
+      alert (`That was a tie! You both picked paper. Find a nice manila folder to spend some time with your new friend in.
+Results as of round ${ round }. Current score: ${ playerWinTotal } to ${ computerWinTotal }`);
+			return;
     }
     else {
-      return "You lose! I'm afraid you're getting cut to ribbons...";
+			computerWinTotal++;
+      alert (`You lose! I'm afraid you're getting cut to ribbons...
+Results as of round ${ round }. Current score: ${ playerWinTotal } to ${ computerWinTotal }`);
+			return computerWinTotal;
     }
   }
   else {
+		computerWinTotal++;
     if (computerWeaponChoice === "rock") {
-      return "You lose! Lmao you got rocked, kid.";
+      alert (`You lose! Lmao you got rocked, kid.
+Results as of round ${ round }. Current score: ${ playerWinTotal } to ${ computerWinTotal }`);
+			return computerWinTotal;
     }
     else if (computerWeaponChoice === "paper") {
-      return "You win! Just think of all the pretty paper snowflakes you can make.";
+			playerWinTotal++;
+      alert(`You win! Just think of all the pretty paper snowflakes you can make.
+Results as of round ${ round }. Current score: ${ playerWinTotal } to ${ computerWinTotal }`);
+			return playerWinTotal;
     }
     else {
-      return "That was a tie! You both picked scissors. Uhhhhh this might get a bit NSFW.";
+      alert (`That was a tie! You both picked scissors. Uhhhhh this might get a bit NSFW.
+Results as of round ${ round }. Current score: ${ playerWinTotal } to ${ computerWinTotal }`);
+			return;
     }
   }
 }
 
+/*
+Function: getPlayerWeapon
+Goals: 		Determine the type of weapon the player wants to use.
+Inputs:		No arguments passed to function.
+					User input string at runtime for their weapon choice.
+Outputs:	String containing user's weapon choice.
+Steps:		Prompt the user for their weapon of choice. R, P, S?
+					Standardize user input by making all responses lower case.
+					Determine if a legal weapon has been chosen.
+					If their weapon choice isn't legal, ask the user to try again.
+					If it is legal, output their choice as a string.
+*/
 function getPlayerWeapon() {
-  while (true) {
-    let playerWeaponChoice = prompt ("Rock, paper, or scissors?", "Rock");
+  let playerWeaponChoice = "";
+	let keepGoing = true;
+	while (keepGoing) {
+    playerWeaponChoice = prompt ("Rock, paper, or scissors?", "Rock");
     // Make sure they have a proper rock, even if they spell it rOcK
     playerWeaponChoice = playerWeaponChoice.toLowerCase();
     // Make sure they don't bring a gun to an arts and crafts fight; if they do, ask until they don't
     if (playerWeaponChoice !== "rock" && playerWeaponChoice !== "paper" && playerWeaponChoice !== "scissors") {
       alert ("You're not trying to cheat, are you? Use a weapon more fitting of a gentleman.");
-      continue;
+			keepGoing = true;
     }
-    return playerWeaponChoice;
+		// Oh hey, they picked a fair weapon!
+		else {
+			keepGoing = false;
+		}
   }
+	return playerWeaponChoice;
 }
 
-let playAgain = true;
+let playerWinTotal = 0;
+let computerWinTotal = 0;
+let round = 1;
 
-while (playAgain) {
-// You pick rock, paper, or scissors - message player for input
-let playerWeaponChoice = getPlayerWeapon();
-// Your friend picks rock, paper, or scissors RANDOMLY - computer generates randomly
-let weaponsChest = ["rock", "paper", "scissors"];
-let computerWeaponChoice = weaponsChest[Math.floor(Math.random()*3)];
-// After figuring out the result, you and your friend play a few more times
-alert (playRound(playerWeaponChoice, computerWeaponChoice));
-playAgain = confirm ("Would you like to play again?");
+// Play five rounds
+for (round; round <= 5; round++) {
+	// Player picks rock, paper, or scissors
+	let playerWeaponChoice = getPlayerWeapon();
+	// Computer picks rock, paper, or scissors RANDOMLY
+	let weaponsChest = ["rock", "paper", "scissors"];
+	let computerWeaponChoice = weaponsChest[Math.floor(Math.random()*3)];
+	// After figuring out the result, you and your friend play a few more times
+	playRound(playerWeaponChoice, computerWeaponChoice);
 }
 
-alert("See you next time!");
+if (playerWinTotal > computerWinTotal) {
+	alert (`With a score of ${ playerWinTotal } to ${ computerWinTotal }, YOU WIN! Minstrels will write songs of praise to honor your glorious conquest.`)
+} 
+else if (playerWinTotal === computerWinTotal) {
+	alert (`With a score of ${ playerWinTotal } to ${ computerWinTotal }, you tie. So close, too!`)
+} 
+else {
+	alert (`With a score of ${ playerWinTotal } to ${ computerWinTotal }, YOU LOSE! YOU WERE SUPPOSED TO BE THE CHOSEN ONE!`)
+}
